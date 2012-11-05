875fda50c7fc09488b19168ad1dfed6c242795ac
João M. M. da Silva <jaodsilv@linux.ime.usp.br>
2012-11-05 18:55:04 -0200

    blur working

--- blur.m
+++ blur.m
@@ -1,1 +1,16 @@
-function b = blur(image)
+function b = blur(image, outputName)
+
+hl = size(image);
+g = image;
+w = [1/16, 2/16, 1/16; 2/16, 4/16, 2/16; 1/16, 2/16, 1/16];
+
+for x = [2:hl(1)-1]
+  for y = [2:hl(2)-1]
+    for s = [-1:1]
+      for t = [-1:1]
+        g(x,y) += w(s+2, t+2)*image(x+s, y+t);
+      endfor
+    endfor
+  endfor
+endfor
+imwrite(g, outputName);
